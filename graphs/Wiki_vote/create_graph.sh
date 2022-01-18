62d5307b2fb79eaa2d8468b7e02b82b93935b99c
samjrx <xujiar@oregonstate.edu>
2022-01-18 14:28:13 -0800

    schema debug for graphs

--- graphs/Wiki_vote/create_graph.sh
+++ graphs/Wiki_vote/create_graph.sh
@@ -1,4 +1,3 @@
 gsql schema_wiki_vote.gsql
 gsql -g wiki_vote load_wiki_vote.gsql
-gsql -g wiki_vote "RUN LOADING JOB load_wiki_vote USING vfile=\"data/v_info.csv\""
 gsql -g wiki_vote "RUN LOADING JOB load_wiki_vote USING efile=\"data/e_list.csv\""
